407aa00b94fbb024d8da79c2d485e56b6b335e4c
myk francis <youraveragecoder@gmail.com>
2022-06-21 12:47:27 +0300

    added coinbase project

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -4,6 +4,7 @@ import realEstate from "../assets/realestate.jpg";
 import movies from "../assets/movies.png";
 import handyman from "../assets/Handyman.png";
 import portfolio from "../assets/homePorto.png";
+import coinbase from "../assets/HomeLight.png";
 
 const Work = () => {
   return (
@@ -90,21 +91,21 @@ const Work = () => {
             </div>
           </div>
           <div
-            style={{ backgroundImage: `url(${realEstate})` }}
+            style={{ backgroundImage: `url(${coinbase})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             {/* Hover Effects */}
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wider">
-                React JS Application
+                CoinBase Application
               </span>
               <div className="pt-8 text-center">
-                <a href="/">
+                <a href="https://github.com/myk-francis/coinbase">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
                 </a>
-                <a href="/">
+                <a href="https://github.com/myk-francis/coinbase">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
