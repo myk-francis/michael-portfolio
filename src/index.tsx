44911c4c42436b7ff7076da068e621e236868c7c
myk <michaelmugendi1707@gmail.com>
2026-02-18 12:38:57 +0300

    Update: added google analytics

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,24 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+const GA_TRACKING_ID = "G-T9KTF0E2GC";
+
+if (process.env.NODE_ENV === "production") {
+  const gaScript = document.createElement("script");
+  gaScript.async = true;
+  gaScript.src = `https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`;
+  document.head.appendChild(gaScript);
+
+  const gaInitScript = document.createElement("script");
+  gaInitScript.innerHTML = `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', '${GA_TRACKING_ID}');
+  `;
+  document.head.appendChild(gaInitScript);
+}
+
 const rootElement = document.getElementById("root");
 
 if (!rootElement) {
